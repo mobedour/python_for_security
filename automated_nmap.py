5da830fd9497bbbc833bb2d13d4df9dc2991b81c
mobedour <75674366+mobedour@users.noreply.github.com>
2024-08-07 11:24:50 +0300

    Add files via upload
    This Python script uses the nmap and netifaces libraries to perform a network scan. It is designed for use on Kali Linux
    or any other system with Python and the required libraries installed. The script automatically detects the local IP 
    address and subnet of the network you are connected to and scans the specified range of ports (1-1024) for open ports 
    and services running on them.
    
    Usage:
    1. Ensure you have Python installed on your system.
    
    2. Install the required libraries if they are not already installed:
       - nmap: `pip install python-nmap`
       - netifaces: `sudo apt-get install python3-netifaces`
    
    3. Run the script using Python:
       - in the terminal, type:
       python3 automated_nmap.py
       
    The script will automatically detect your local IP address and subnet, perform a scan on the network, and display
    the results, including the host IP addresses, states, and services detected.
    Note: This script requires administrative privileges to run correctly. Make sure to run it with appropriate permissions.
    
    Author: mobedour

---
+++ automated_nmap.py
@@ -0,0 +1,66 @@
+"""
+This Python script uses the nmap and netifaces libraries to perform a network scan. It is designed for use on Kali Linux
+or any other system with Python and the required libraries installed. The script automatically detects the local IP 
+address and subnet of the network you are connected to and scans the specified range of ports (1-1024) for open ports 
+and services running on them.
+
+Usage:
+1. Ensure you have Python installed on your system.
+
+2. Install the required libraries if they are not already installed:
+   - nmap: `pip install python-nmap`
+   - netifaces: `sudo apt-get install python3-netifaces`
+
+3. Run the script using Python:
+   - in the terminal, type:
+   python3 automated_nmap.py
+   
+The script will automatically detect your local IP address and subnet, perform a scan on the network, and display
+the results, including the host IP addresses, states, and services detected.
+Note: This script requires administrative privileges to run correctly. Make sure to run it with appropriate permissions.
+
+Author: mobedour
+"""
+
+import nmap
+import socket
+import netifaces
+
+def get_ip_address():
+    interfaces = netifaces.interfaces()
+    for interface in interfaces:
+        addrs = netifaces.ifaddresses(interface)
+        if netifaces.AF_INET in addrs:
+            ip_info = addrs[netifaces.AF_INET][0]
+            ip_address = ip_info['addr']
+            netmask = ip_info['netmask']
+            if ip_address != '127.0.0.1':
+                return ip_address, netmask
+    return None, None
+
+def get_network_range(ip_address, netmask):
+    ip_parts = ip_address.split('.')
+    mask_parts = netmask.split('.')
+    network_parts = [str(int(ip_parts[i]) & int(mask_parts[i])) for i in range(4)]
+    network_range = '.'.join(network_parts) + '/24'
+    return network_range
+
+def scan_network(ip_range):
+    nm = nmap.PortScanner()
+    nm.scan(ip_range, '1-1024', '-sV')
+    for host in nm.all_hosts():
+        print(f'Host: {host} ({nm[host].hostname()})')
+        print(f'State: {nm[host].state()}')
+        for proto in nm[host].all_protocols():
+            print(f'Protocol: {proto}')
+            lport = nm[host][proto].keys()
+            for port in lport:
+                print(f'port: {port}\tstate: {nm[host][proto][port]["state"]}\tservice: {nm[host][proto][port]["name"]}')
+
+ip_address, netmask = get_ip_address()
+if ip_address and netmask:
+    network_range = get_network_range(ip_address, netmask)
+    print(f'Scanning network: {network_range}')
+    scan_network(network_range)
+else:
+    print('Could not determine local IP address and netmask.')
